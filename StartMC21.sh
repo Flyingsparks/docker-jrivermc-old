137e822d7e413fe4fb622b3ddee02e22de07e17f
Flyingsparks <stefan.raabe@gmail.com>
2016-03-06 19:45:23 +0100

    updated to use tightvnc

--- StartMC21.sh
+++ StartMC21.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export USER=pi
-ps -e | grep x11vnc || x11vnc :0 -localhost -geometry 1920x1080
-export DISPLAY=':0'
+ps -e | grep tightvnc || vncserver :1 -geometry 1920x1080
+export DISPLAY=':1'
 ps -e | grep mediacenter || mediacenter21 /mediaserver
